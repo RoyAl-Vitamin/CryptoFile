550caef88fa3627004eb4c6b2e9db9aec12025e9
RoyAl-Vitamin <roalvi95@gmail.com>
2023-01-20 19:35:51 +0500

    Add generated table

--- src/main/java/vi/al/ro/service/DataBaseService.java
+++ src/main/java/vi/al/ro/service/DataBaseService.java
@@ -1,65 +1,54 @@
 package vi.al.ro.service;
 
 import lombok.extern.log4j.Log4j2;
-import org.jooq.DSLContext;
+import org.jooq.CloseableDSLContext;
 import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.codegen.GenerationTool;
 import org.jooq.impl.DSL;
 import vi.al.ro.mapper.KeyStoreMapper;
 import vi.al.ro.model.KeyStoreEntity;
 import vi.al.ro.model.db.tables.records.KeyStoreRecord;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import static org.jooq.impl.SQLDataType.BIGINT;
+import static org.jooq.impl.SQLDataType.VARCHAR;
 import static vi.al.ro.model.db.Tables.KEY_STORE;
 
 @Log4j2
 public class DataBaseService {
 
 //    private static final String DB_URL = "jdbc:h2:mem:crypto"; // In memory db
-    private static final String DB_URL = "jdbc:h2:./crypto;CIPHER=AES"; // With encryption
 //    private static final String DB_URL = "jdbc:h2:./crypto"; // Without encryption
+    private static final String DB_URL = "jdbc:h2:./crypto;CIPHER=AES"; // With encryption
 
     private static final String PASSWORD = "password user_password";
 
     private static final String USER = "user";
 
-    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS key_store(id BIGINT PRIMARY KEY, alias VARCHAR(255), password VARCHAR(255), path_file VARCHAR(255) UNIQUE);";
-
-    private final Connection connection;
-
-    private final DSLContext context;
+    private final CloseableDSLContext context;
 
     private static final DataBaseService service = new DataBaseService();
 
     private DataBaseService() {
         try {
-            // TODO заменить создание таблиц на liquibase or jooq
             Class.forName("org.h2.Driver");
-            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            try (Statement statement = connection.createStatement();) {
-                statement.execute(CREATE_TABLE);
-            }
-//            DSL.using("jdbc:url:something", "username", "password").close();
-            context = DSL.using(connection, SQLDialect.H2);
-
-            Path path = Paths.get(Objects.requireNonNull(DataBaseService.class.getClassLoader().getResource("jooq-config.xml")).toURI());
-            GenerationTool.generate(Files.readString(path));
         } catch (Exception e) {
             log.error("", e);
             throw new RuntimeException(e);
         }
+
+        context = DSL.using(DB_URL, USER, PASSWORD);
+        context.createTableIfNotExists("key_store")
+                .column("id", BIGINT)
+                .column("alias", VARCHAR(255))
+                .column("password", VARCHAR(255))
+                .column("path_file", VARCHAR(255))
+                .primaryKey("id")
+                .unique("path_file")
+                .execute();
     }
 
     public int save(String alias, String password, String pathToFile) throws SQLException {
@@ -82,10 +71,6 @@ public class DataBaseService {
     }
 
     public static void close() {
-        try {
-            service.connection.close();
-        } catch (SQLException e) {
-            log.error("", e);
-        }
+        service.context.close();
     }
 }
