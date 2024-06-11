a770bbda014fd685323abb753d180013de0da5e6
RoyAl-Vitamin <roalvi95@gmail.com>
2024-06-11 18:54:00 +0500

    Initialize tabs

--- src/main/java/vi/al/ro/controller/MainController.java
+++ src/main/java/vi/al/ro/controller/MainController.java
@@ -11,49 +11,60 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Map;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
     private static final Logger log = LogManager.getLogger(MainController.class);
 
+    private static final URL XML_ENCRYPT_URL = MainController.class.getResource("fxmlEncrypt.fxml");
+
+    private static final URL XML_DECRYPT_URL = MainController.class.getResource("fxmlDecrypt.fxml");
+
+    private static final URL XML_KEY_STORE_URL = MainController.class.getResource("fxmlKeyStore.fxml");
+
+    @FXML
+    private Tab tabDecrypt;
+
+    @FXML
+    private Tab tabEncrypt;
+
+    @FXML
+    private Tab tabKeyStore;
+
     @FXML
     private TabPane tabPane;
 
-    private static final Map<String, String> TAB_MAP = Map.of("Encrypt", "fxmlEncrypt.fxml", "Decrypt", "fxmlDecrypt.fxml", "Key Store", "fxmlKeyStore.fxml");
-
     @Override
     public void initialize(URL url1, ResourceBundle resourceBundle) {
-        // It is important to call it before adding ChangeListener to the tabPane to avoid NPE and
-        // to be able to fire the manual selection event below. Otherwise, the 1st tab will be selected
-        // with empty content.
-        tabPane.getSelectionModel().clearSelection();
+        tabEncrypt.setContent(getEncryptParent());
+        tabDecrypt.setContent(getDecryptParent());
+        tabKeyStore.setContent(getKeyStoreParent());
+    }
 
-        // Add only tabs dynamically but not their content
-        for (Map.Entry<String, String> entry : TAB_MAP.entrySet()) {
-            tabPane.getTabs().add(new Tab(entry.getKey()));
+    private Parent getEncryptParent() {
+        return getParent(XML_ENCRYPT_URL, new EncryptionController());
+    }
+
+    private Parent getDecryptParent() {
+        return getParent(XML_DECRYPT_URL, new DecryptionController());
+    }
+
+    private Parent getKeyStoreParent() {
+        return getParent(XML_KEY_STORE_URL, new KeyStoreController());
+    }
+
+    private Parent getParent(URL fxmlUrl, Object controller) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(fxmlUrl);
+        loader.setController(controller);
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            log.error("", e);
+            throw new RuntimeException(e);
         }
-
-        // Add Tab ChangeListener
-        tabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.getContent() == null) {
-                try {
-                    // Loading content on demand
-                    URL url = MainController.class.getResource(TAB_MAP.get(newValue.getText()));
-                    Parent root = (Parent) FXMLLoader.load(Objects.requireNonNull(url));
-                    newValue.setContent(root);
-                } catch (IOException e) {
-                    log.error("", e);
-                }
-            } else {
-                // Content is already loaded. Update it if necessary.
-                Parent root = (Parent) newValue.getContent();
-                // Optionally get the controller from Map and manipulate the content
-                // via its controller.
-            }
-        });
-        tabPane.getSelectionModel().selectFirst();
+        return root;
     }
 }
