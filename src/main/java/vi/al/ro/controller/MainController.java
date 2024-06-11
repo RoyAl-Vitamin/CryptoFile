bfc4449950ee5bab1310ec9ca44af01ce3012e03
RoyAl-Vitamin <roalvi95@gmail.com>
2024-06-11 18:56:47 +0500

    Code refactor

--- src/main/java/vi/al/ro/controller/MainController.java
+++ src/main/java/vi/al/ro/controller/MainController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,12 +16,6 @@ public class MainController implements Initializable {
 
     private static final Logger log = LogManager.getLogger(MainController.class);
 
-    private static final URL XML_ENCRYPT_URL = MainController.class.getResource("fxmlEncrypt.fxml");
-
-    private static final URL XML_DECRYPT_URL = MainController.class.getResource("fxmlDecrypt.fxml");
-
-    private static final URL XML_KEY_STORE_URL = MainController.class.getResource("fxmlKeyStore.fxml");
-
     @FXML
     private Tab tabDecrypt;
 
@@ -32,9 +25,6 @@ public class MainController implements Initializable {
     @FXML
     private Tab tabKeyStore;
 
-    @FXML
-    private TabPane tabPane;
-
     @Override
     public void initialize(URL url1, ResourceBundle resourceBundle) {
         tabEncrypt.setContent(getEncryptParent());
@@ -43,15 +33,15 @@ public class MainController implements Initializable {
     }
 
     private Parent getEncryptParent() {
-        return getParent(XML_ENCRYPT_URL, new EncryptionController());
+        return getParent(MainController.class.getResource("fxmlEncrypt.fxml"), new EncryptionController());
     }
 
     private Parent getDecryptParent() {
-        return getParent(XML_DECRYPT_URL, new DecryptionController());
+        return getParent(MainController.class.getResource("fxmlDecrypt.fxml"), new DecryptionController());
     }
 
     private Parent getKeyStoreParent() {
-        return getParent(XML_KEY_STORE_URL, new KeyStoreController());
+        return getParent(MainController.class.getResource("fxmlKeyStore.fxml"), new KeyStoreController());
     }
 
     private Parent getParent(URL fxmlUrl, Object controller) {
